ee7ac0e9f9c1c162610a9abe3c408543f1dc5ad3
Francesco Mancuso <francesco.mancuso@anotherweb.it>
2025-10-08 21:41:40 +0200

    Aggiunto gitignore, inserite ottimizzazioni grafiche e piccole funzioni extra

--- js/script.js
+++ js/script.js
@@ -9,3 +9,34 @@ function scrollHeader() {
 
 window.addEventListener("scroll", scrollHeader);
 window.addEventListener("load", scrollHeader);
+
+function scrollToTop() {
+    const b = document.getElementById("totop");
+    if (window.scrollY > 100) {
+        b.classList.add("active");
+    } else {
+        b.classList.remove("active");
+    }
+}
+
+window.addEventListener("scroll", scrollToTop);
+window.addEventListener("load", scrollToTop);
+
+document.getElementById("totop").addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+function preloader() {
+    const l = document.getElementById("loader");
+    setTimeout(() => {
+        l.classList.add("ready");
+    }, 1500);
+}
+
+window.addEventListener("load", preloader);
+
+setTimeout(() => {
+    const l = document.getElementById("loader");
+    l.classList.add("ready");
+}, 10000);
